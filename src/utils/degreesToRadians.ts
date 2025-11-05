e69d42c9145e6383adac516be90a2ffff920f4a0
Christian Holt <sidaroth@protonmail.com>
2025-11-05 19:20:05 +0100

    Add util degreesToRadians

---
+++ src/utils/degreesToRadians.ts
@@ -0,0 +1,8 @@
+/**
+ * Converts a value from degrees to radians.
+ * @param degrees The value in degrees.
+ * @returns The value in radians.
+ */
+export function degreesToRadians(degrees: number): number {
+    return degrees * Math.PI / 180;
+}
