ec98181b3a96b334d20e95f5eaecf070313ffd16
Christian Holt <sidaroth@protonmail.com>
2025-11-06 11:30:45 +0100

    Refactor line to conscise JSDoc comments where possible.

--- src/geometry/Line.ts
+++ src/geometry/Line.ts
@@ -1,5 +1,4 @@
-import { Vector } from '../vector';
-import { Point } from '../core';
+import { Point, Vector } from '../core';
 import { Geometry } from './Geometry';
 
 /**
@@ -29,85 +28,63 @@ export class Line extends Geometry {
         this.refresh();
     }
 
-    /**
-     * @returns The origin point of the line.
-     */
+    /** @returns The origin point of the line. */
     get origin() {
         this.ensureRefreshed();
         return this._origin;
     }
 
-    /**
-     * @returns The end point of the line.
-     */
+    /** @returns The end point of the line. */
     get end() {
         this.ensureRefreshed();
         return this._end;
     }
 
-    /**
-     * @returns A vector representation from the origin to the end of the line.
-     */
+    /** @returns A vector representation from the origin to the end of the line. */
     get vector(): Vector {
         this.ensureRefreshed();
         return this._vector;
     }
 
-    /**
-     * @returns The length of the line.
-     */
+    /** @returns The length of the line. */
     get length(): number {
         this.ensureRefreshed();
         return this._length;
     }
 
-    /**
-     * @returns The midpoint of the line.
-     */
+    /** @returns The midpoint of the line. */
     get midpoint(): Point {
         this.ensureRefreshed();
         return this._midpoint;
     }
 
-    /**
-     * @returns The angle of the line.
-     */
+    /** @returns The angle of the line. */
     get angle(): number {
         this.ensureRefreshed();
         return this._angle;
     }
 
-    /**
-     * Calculates the length of the line.
-     */
+    /** Calculates the length of the line. */
     private calculateLength() {
         this._length = this._origin.distance(this._end);
     }
 
-    /**
-     * Calculates the midpoint of the line.
-     */
+    /** Calculates the midpoint of the line. */
     private calculateMidpoint() {
         this._midpoint.copyFrom(this._origin.midpoint(this._end));
     }
 
-    /**
-     * Calculates the angle of the line.
-     */
+    /** Calculates the angle of the line. */
     private calculateAngle() {
         this._angle = this._origin.angle(this._end);
     }
 
-    /**
-     * Calculates the vector of the line.
-     */
+    /** Calculates the vector of the line. */
     private calculateVector() {
         this._vector.set(this._end).sub(this._origin);
     }
 
-    /**
-     * Refreshes the line's properties.
-     */
+    /** Refreshes the line's properties. */
     protected refresh() {
         this.calculateLength();
         this.calculateMidpoint();
@@ -115,16 +92,12 @@ export class Line extends Geometry {
         this.calculateVector();
     }
 
-    /**
-     * @returns A string representation of the line.
-     */
+    /** @returns A string representation of the line. */
     override toString() {
         return `Line(origin: ${this._origin.toString()}, end: ${this._end.toString()})`;
     }
 
-    /**
-     * @returns A deep copy of the line.
-     */
+    /** @returns A deep copy of the line. */
     clone() {
         return new Line(this._origin.clone(), this._end.clone());
     }
