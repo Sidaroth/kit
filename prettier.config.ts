6084ab73a84d48147887b8d44e42e785703afe8c
Christian Holt <sidaroth@protonmail.com>
2025-11-06 09:56:38 +0100

    Set up linting and building

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'prettier';
+
+const config: Config = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+};
+
+export default config;
