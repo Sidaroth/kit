01b76aef28aa138fd298eb7d0417b7bc16cc3d30
Christian Holt <sidaroth@protonmail.com>
2025-11-07 09:51:33 +0100

    Narrow size typeguard to only allow finite numbers

--- src/core/size.ts
+++ src/core/size.ts
@@ -3,8 +3,18 @@ export interface Size {
     height: number;
 }
 
+/**
+ * Checks if a given object is a Size object. Could theoretically limit it to positive numbers only.
+ * But that would limits its usefulness in other math applications.
+ * @param size - The size to check.
+ * @returns True if the size is a valid Size object, false otherwise.
+ */
 export function isSize(size: unknown): size is Size {
     if (!size) return false;
+    if (typeof size !== 'object') return false;
 
-    return typeof size === 'object' && 'width' in size && 'height' in size;
+    return (
+        Number.isFinite((size as Size).width) &&
+        Number.isFinite((size as Size).height)
+    );
 }
